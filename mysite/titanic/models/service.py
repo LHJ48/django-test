d043387ffe921b815e5305c6cd911cd6ab373fdf
LeeHoJun4880 <ghwns23@gmail.com>
2021-06-12 13:53:32 +0900

    second commit

---
+++ mysite/titanic/models/service.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+from mysite.titanic.models.dataset import Dataset
+
+class Service(object):
+
+    dataset = Dataset()
+
+    def new_model(self, payload) -> object:
+        this = self.dataset
+        this.context = './data/'
+        this.fname = payload
+        return pd.read_csv(this.context + this.fname)
+
