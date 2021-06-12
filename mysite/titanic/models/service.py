52e8cd24721b04f0de8aec45cde767d45fa30340
LeeHoJun4880 <ghwns23@gmail.com>
2021-06-12 16:31:36 +0900

    fifth commit

--- mysite/titanic/models/service.py
+++ mysite/titanic/models/service.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from mysite.titanic.models.dataset import Dataset
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
 
 class Service(object):
 
@@ -11,4 +14,87 @@ class Service(object):
         this.context = '../data/'
         this.fname = payload
         return pd.read_csv(this.context + this.fname)
+
+    @staticmethod
+    def create_label(this) -> object:
+        return this.train.drop('Survived', axis = 1) # axis 0 가로, 1 세로
+
+    @staticmethod
+    def create_train(this) -> object:
+        return this.train['Survived']
+
+    @staticmethod
+    def drop_feature(this, *feature) -> object:
+        for i in feature:
+            this.train = this.train.drop([i], axis = 1)
+            this.test = this.test.drop([i], axis = 1)
+            # 학습, 테스트 세트는 항상 동일하게 편집한다.
+
+    @staticmethod
+    def embarked_nominal(this) -> object:
+        this.train = this.train.fillna({'Embraked', 'S'}) # S는 사우스햄튼
+        this.test = this.test.fillna({'Embraked', 'S'}) # S는 사우스햄튼
+        this.train['Embarked'] = this.train['Embarked'].map({'S':1, 'C':2, 'Q':3 })
+        this.test['Embarked'] = this.test['Embarked'].map({'S':1, 'C':2, 'Q':3 })
+        return  this
+
+    @staticmethod
+    def title_norminal(this) -> object:
+        combine = [this.train, this.test]
+        for dataset in combine:
+            dataset['Title'] = dataset.Name.str.extract('([A-Za-z]+)\.', expand=False)
+        for dataset in combine:
+            dataset['Title'] = dataset['Title'].replace(
+                ['Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Jonkheer', 'Dona'], 'Rare')
+            dataset['Title'] = dataset['Title'].replace(['Countess', 'Lady', 'Sir'], 'Royal')
+            dataset['Title'] = dataset['Title'].replace('Mlle', 'Mr')
+            dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+            dataset['Title'] = dataset['Title'].replace('Mme', 'Rare')
+            title_mapping = {'Mr': 1, 'Miss': 2, 'Mrs': 3, 'Master': 4, 'Royal': 5, 'Rare': 6}
+            dataset['Title'] = dataset['Title'].fillna(0)
+            dataset['Title'] = dataset['Title'].m
+        return this
+
+    @staticmethod
+    def gender_norminal(this) -> object:
+        combine = [this.train, this.test]
+        gender_mapping = {'male': 0, 'female': 1}
+        for i in combine:
+            i['Gender'] = i['Sex'].map(gender_mapping)
+
+    @staticmethod
+    def age_original(this) -> object:
+        train = this.train
+        test = this.test
+        for data in train, test:
+            data['Age'] = data['Age'].fillna(-0.5)
+        bins = [-1, 0, 5, 12, 18, 24, 35, 68, np.inf]
+        labels = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
+        age_title_mapping = {'Unknown': 0, 'Baby': 1, 'Child': 2, 'Teenager': 3, 'Student': 4,
+                             'Young Adult': 5, 'Adult': 6, 'Senior': 7}
+        for data in train, test:
+            data['AgeGroup'] = pd.cut(data['Age'], bins=bins, labels=labels)
+            data['AgeGroup'] = data['AgeGroup'].map(age_title_mapping)
+        return this
+
+    @staticmethod
+    def fare_ordinal(this) -> object:
+        this.test['Fare'] = this.test['Fare'].fillna(1)
+        this.train['FareBand'] = pd.qcut(this.train['Fare'], 4, labels={1,2,3,4}) #최고와 최저를 통해 4등분하라
+
+    @staticmethod
+    def create_k_fold() -> object:
+        return KFold(n_splits=10,shuffle=True, random_state=0) #트레인데이터를 10등분, 반복출제 허용
+
+    def get_accurcy(self, this):
+        score = cross_val_score(RandomForestClassifier(),
+                                this.train,
+                                this.label,
+                                cv=self.create_k_fold(),
+                                n_jobs=1,
+                                scoring='accuracy')
+        return round(np.mean(score)*100, 2)
+
+
+
 
